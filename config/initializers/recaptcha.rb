c552e79918bef4cddf16b32aab114ac849ab0ae7
Noel <noel@2bedigital.com>
2015-07-27 23:50:46 +0200

    delete initializers

--- config/initializers/recaptcha.rb
+++
@@ -1,9 +0,0 @@
-Recaptcha.configure do |config|
-  config.public_key  = '6LdPRQoTAAAAA-xxxxxxxxxxxxxxxxxxxxxxxxxx'
-  config.private_key = '6LdPRQoTAAAAA-yyyyyyyyyyyyyyyyyyyyyyyyyy'
-  # Uncomment the following line if you are using a proxy server:
-  # config.proxy = 'http://0.0.0.0:3001'
-  # Uncomment if you want to use the newer version of the API,
-  # only works for versions >= 0.3.7:
-  # config.api_version = 'v2'
-end
