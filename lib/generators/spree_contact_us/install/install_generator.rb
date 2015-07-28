7ac832368ec938ff6542a9a0cb2ad84a016f4311
Noel <noeldiaz83@gmail.com>
2015-07-28 13:42:49 +0100

    modified stylesheets

--- lib/generators/spree_contact_us/install/install_generator.rb
+++ lib/generators/spree_contact_us/install/install_generator.rb
@@ -10,7 +10,7 @@ module SpreeContactUs
       end
 
       def add_stylesheets
-        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/contact_us\n", :before => /\*\//, :verbose => true
+        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/spree_contact_us\n", :before => /\*\//, :verbose => true
       end
 
       def copy_initializer_file
